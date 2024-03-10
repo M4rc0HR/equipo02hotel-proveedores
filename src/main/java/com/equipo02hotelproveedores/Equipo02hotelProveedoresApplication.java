b36dce4b6e4fcb6a9f5941d7c8a645852e00236b
M4rc0HR <mhuamanreyes@gmail.com>
2024-03-10 12:55:43 -0500

    Configuracion de microservico Proveedores

---
+++ src/main/java/com/equipo02hotelproveedores/Equipo02hotelProveedoresApplication.java
@@ -0,0 +1,13 @@
+package com.equipo02hotelproveedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo02hotelProveedoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Equipo02hotelProveedoresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/equipo02hotelproveedores/Equipo02hotelProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo02hotelproveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo02hotelProveedoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
